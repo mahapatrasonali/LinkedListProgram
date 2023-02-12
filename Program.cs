7b515f610fc9ffe4643b7b58c2074aaed69a8357
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-02-12 20:45:31 +0530

    Linked List

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LinkedListProgram
+{
+    class Program
+    {
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome To ");
+            Console.WriteLine("Choose a option from below\n" +
+                "Choose 1): Creating a linked list ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    LinkedList linkedList = new LinkedList();
+                    linkedList.Add(56);
+                    linkedList.Add(30);
+                    linkedList.Add(30);
+                    linkedList.Add(70);
+                    linkedList.Display();
+                    break;
+                default:
+                    Console.WriteLine("Choose a Valid Option");
+                    break;
+            }
+        }
+    }
+}
